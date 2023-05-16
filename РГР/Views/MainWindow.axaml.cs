696b143e8850a3df2ac44624e2563264f1873ebf
Denchikcool <kuzmenok.den@yandex.ru>
2023-05-16 15:22:36 +0700

    Создание первых окон

---
+++ РГР/Views/MainWindow.axaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.ComponentModel;
+
+namespace РГР.Views
+{
+    public partial class MainWindow : Window
+    {
+
+        private Programm programm;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Exit_programm(object sender, RoutedEventArgs eventArgs)
+        {
+            this.Close();
+        }
+
+        public void Exit_programm2(object? sender, CancelEventArgs e)
+        {
+            this.Close();
+        }
+
+        public void Create_Programm(object sender, RoutedEventArgs eventArgs)
+        {
+            programm= new Programm();
+            this.Hide();
+            programm.Show();
+            programm.Closing += Exit_programm2;
+        }
+    }
+}
---
+++ РГР/Views/Programm.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace РГР.Views
+{
+    public partial class Programm : Window
+    {
+        private MainWindow mainwindow;
+        public Programm()
+        {
+            InitializeComponent();
+        }
+
+        //public WellcomeWindow(MainWindow sender)
+        //{
+        //    InitializeComponent();
+        //    mainwindow = sender;
+        //    //mainwindow.Hide();
+        //}
+
+        //public void Exit_programm(object sender, RoutedEventArgs eventArgs)
+        //{
+        //    this.Close();
+        //    mainwindow.Close();
+        //}
+    }
+}
